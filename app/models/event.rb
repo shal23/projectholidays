779a0ee702d542c7eab8012b8a43ca8063137098
shal23 <dr.shalinidalal@gmail.com>
2013-06-11 16:21:47 +1000

    maps for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,14 @@
 class Event < ActiveRecord::Base
   has_event_calendar
 
-  attr_accessible :name, :start_at, :end_at, :description
+  attr_accessible :name, :start_at, :end_at, :description, :address
 
-  default_scope order: 'events.created_at DESC'
+  default_scope order: 'events.start_at DESC'
+
+  acts_as_gmappable
+
+	def gmaps4rails_address
+		#describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+			"#{self.address}" 
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611023555) do
+ActiveRecord::Schema.define(:version => 20130611054726) do
 
   create_table "agent_profiles", :force => true do |t|
     t.string   "name"
@@ -70,6 +70,10 @@ ActiveRecord::Schema.define(:version => 20130611023555) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.text     "description"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.boolean  "gmaps"
   end
 
   create_table "posts", :force => true do |t|
