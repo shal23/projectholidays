bd667c9d946ef8ba5dc63083247d47e7d8c6e14d
shal23 <dr.shalinidalal@gmail.com>
2013-06-20 13:46:13 +1000

    fix roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,13 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
+    else
+      can :read, :all
+    end
+
+
+    if user.has_role? :agent
+        can :create, :events, :deals
     end
     # Define abilities for the passed in user here. For example:
     #
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
+  attr_accessible :role_ids, :as => :agent
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :agent
 
   has_many :posts
